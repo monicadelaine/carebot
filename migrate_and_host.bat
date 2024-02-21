f4b42d53bfd3614860018fba8052a69867096cbe
KyberCritter <88512946+KyberCritter@users.noreply.github.com>
2024-02-20 21:50:43 -0600

    displays generic error pages when debug is false (cannot display in debug mode)

--- migrate_and_host.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM Set up and run migrations
-pipenv run python carebot/manage.py makemigrations
-pipenv run python carebot/manage.py migrate
-
-REM Start the development server
-pipenv run python carebot/manage.py runserver
