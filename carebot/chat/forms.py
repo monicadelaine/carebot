ff5fa44bca1e48990a3c252ed9dc699c4e9c74a5
qcflanigan <qcflanigan@crimson.ua.edu>
2024-03-25 15:15:24 -0400

    Expanding user input (changed from textForm to textarea) had to adjust how suggested msg buttons send and added event listener to ensure text area sends on "enter" key

--- carebot/chat/forms.py
+++ carebot/chat/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class QueryForm(forms.Form):
     query = forms.CharField(
         max_length=1000,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type your query here...'})
+        widget=forms.Textarea(attrs={'id': 'user-query', 'rows': '1', 'class': 'form-control', 'placeholder': 'Type your query here...'})
     )
     #is_sql = forms.BooleanField(required=False)  # Sql checkbox that can be removed, kept for dev purposes
